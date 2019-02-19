228099a356f83a7e07e7d6862b3e338d4f29d6a3
MarkoRaseta <marko.raseta@moj-posao.net>
2019-02-19 13:11:56 +0100

    - dobili lattitude

--- src/index.js
+++ src/index.js
@@ -5,22 +5,34 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		//only time we do direct assigment to this.state
-		this.state = {lat: null};
+		this.state = {lat: null, errorMessage: ''};
 
 		window.navigator.geolocation.getCurrentPosition(
 			position => {
-				//we call setState
+				//we call setState to update state
 				this.setState({lat: position.coords.latitude});
 			},
-			err => console.log(err)
+			err => {
+				this.setState({errorMessage: err.message})
+			}
 		);
 	}
 
 	//render is requirement
 	render() {
-		return (
-			<div>Lattitude: {this.state.lat}</div>
-		);
+		if(this.state.errorMessage && !this.state.lat){
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if(!this.state.errorMessage && this.state.lat){
+			return <div>Latitude: {this.state.lat}</div>;
+		}
+
+		if(!this.state.errorMessage && !this.state.lat){
+			return <div>Loading..</div>;
+		}
+
+
 	}
 }
 
