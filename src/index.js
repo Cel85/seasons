0d6330efcd814708abfd8b1256a003ab2608a2c2
MarkoRaseta <marko.raseta@moj-posao.net>
2019-02-25 13:17:31 +0100

    SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from  'react-dom';
 
 class App extends React.Component {
-	state = {lat: null, errorMessage: ''};
+	state = {lat: null, errorMessage: ''};//default values
 
 	componentDidMount(){
 		//also, do some API requests
